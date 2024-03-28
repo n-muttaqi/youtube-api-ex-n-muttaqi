d1c4064b273fd21b3589a5ffdd1eff155013ca89
Turjo Muttaqi <nmuttaqi@emich.edu>
2024-03-28 03:06:53 -0400

    Return list of videos Returned of videos.
    Issue: #3

--- youtube.py
+++ youtube.py
@@ -1,9 +1,9 @@
+# youtube.py
 import sys
 import config
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
-# Set up API key and service parameters
 DEVELOPER_KEY = config.API_KEY
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
@@ -11,7 +11,6 @@ YOUTUBE_API_VERSION = "v3"
 def youtube_search(query_term, max_results):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
 
-    # Call the search.list method to retrieve results matching the specified query term.
     search_response = youtube.search().list(
         q=query_term,
         part="snippet",
@@ -19,15 +18,13 @@ def youtube_search(query_term, max_results):
         maxResults=max_results
     ).execute()
 
-    return search_response
+    videos = search_response.get("items", [])
+
+    return videos
 
 if __name__ == "__main__":
-    # Prompt the user for necessary input
     query_term = input("Enter the search query: ")
     max_results = int(input("Enter the maximum number of results: "))
-
-    # Perform the YouTube search
-    search_response = youtube_search(query_term, max_results)
-
-    # Print the search response
-    print(search_response)
+    video_list = youtube_search(query_term, max_results)
+    
+    print(video_list)
