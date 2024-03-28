8363845d32a34af9864fe7a5b6ebdf78b18e9a16
Turjo Muttaqi <nmuttaqi@emich.edu>
2024-03-28 03:11:18 -0400

    Add loop Added feature to youtube.py so that it prints 5 pages using a loop
    Issue: #5

--- youtube.py
+++ youtube.py
@@ -1,5 +1,7 @@
+# youtube.py
 import sys
 import config
+import json
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
@@ -7,37 +9,32 @@ DEVELOPER_KEY = config.API_KEY
 YOUTUBE_API_SERVICE_NAME = "youtube"
 YOUTUBE_API_VERSION = "v3"
 
-def youtube_search(query_term, max_results, page_token=None):
+def youtube_search(query_term, max_results):
     youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION, developerKey=DEVELOPER_KEY)
+    all_videos = []
 
-    search_response = youtube.search().list(
-        q=query_term,
-        part="snippet",
-        type="video",
-        maxResults=max_results,
-        pageToken=page_token  
-    ).execute()
+    next_page_token = None
+    for _ in range(5): 
+        search_response = youtube.search().list(
+            q=query_term,
+            part="snippet",
+            type="video",
+            maxResults=max_results,
+            pageToken=next_page_token 
+        ).execute()
 
-    videos = search_response.get("items", [])
-    next_page_token = search_response.get("nextPageToken")
+        videos = search_response.get("items", [])
+        all_videos.extend(videos)
 
-    return videos, next_page_token
+        next_page_token = search_response.get("nextPageToken")
+        if not next_page_token:
+            break 
+
+    return all_videos
 
 if __name__ == "__main__":
-
     query_term = input("Enter the search query: ")
-    max_results = int(input("Enter the maximum number of results: "))
+    max_results = int(input("Enter the maximum number of results per page: "))
+    all_videos = youtube_search(query_term, max_results)
 
-    first_page_videos, next_page_token = youtube_search(query_term, max_results)
-
-    print("First Page Results:")
-    for video in first_page_videos:
-        print(video)
-
-    if next_page_token:
-
-        second_page_videos, _ = youtube_search(query_term, max_results, next_page_token)
-
-        print("\nSecond Page Results:")
-        for video in second_page_videos:
-            print(video)
+    print(json.dumps(all_videos, indent=2))
